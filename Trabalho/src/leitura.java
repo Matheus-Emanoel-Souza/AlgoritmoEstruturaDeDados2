8545ade0e069ac90ed011e22c9f38e2a2334e8dd
Matheus Emanoel <matheusemanoel@ucl.br>
2025-03-15 18:48:04 -0300

    Teste concluido!
    Agregação de dados em Array devidamente feitos!

--- Trabalho/src/leitura.java
+++ Trabalho/src/leitura.java
@@ -4,14 +4,18 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class leitura {
-	 public static void main(String[] args) {
+	 public static void main(String[] args) throws IOException {
+		 
 		 int linhas = 80;
 		 //estou colocando um tamanho fixo para não precisar repetir o processo de leitura do arquivo.
 		 //Há alguma forma de saber o tamanho do arquivo sem precisar percorre-lo ?
+		 //Scanner só é util se for informar pasta pelo console ?
 		 
-	        Scanner scanner = new Scanner(System.in);
-	        System.out.print("Digite o caminho do arquivo: ");
-	        String caminhoArquivo = scanner.nextLine();
+	        //Scanner scanner = new Scanner(System.in);
+	       // System.out.print("Digite o caminho do arquivo: ");
+	        //String caminhoArquivo = scanner.nextLine();
+	       String caminhoArquivo = "C:\\Users\\mathe\\Desktop\\Trabalho.txt";
+	       
 	          
 	        //Array que vão separar as inf. dos alunos.
 	        String[] NomesDosAlunos = new String[linhas];
@@ -28,7 +32,7 @@ public class leitura {
 	                //System.out.println(linha);
 	            	//Array que vai dividir as linhas.
 	            	String[] partes = linha.split(";");
-	            	if(partes.length == 2) {
+	            	if(partes.length == 5) {
 	            		NomesDosAlunos[contador] = partes[0].trim();
 	            		Curso[contador] = partes[1].trim();
 	            		Disciplina[contador] = partes[2].trim();
@@ -38,23 +42,20 @@ public class leitura {
 	            	else{
 	            	System.out.println("Algo no arquivo deu errado.");
 	            	}
-	            	
-	            	}
-	            	
-	            		            	
+	            	contador++;
+	            	//System.out.println(contador +"a "+ "Aluno registrado!");
+	            	}	            	            		            		            	
 	            }
-	        } 
-	        catch (IOException e) {
+	        catch (NumberFormatException e) {
             System.out.println("Erro ao ler o arquivo: " + e.getMessage());
-        }
-	        scanner.close();
-	        //Contagem de linhas do arquivo.
-	        
-	        
-	        
-	        
-	    }
-	}
+	        }
+	        //teste para ver se array está funcionando.
+	        for(int i = 0; i<contador ;i++) {
+            	System.out.println(NomesDosAlunos[i]);	        	
+            }
+	 }
+}
+
 //1a parte ->Ler arquivos.
-//escrever -> C:\Users\mathe\Desktop\80notas.txt
+//escrever -> C:\Users\mathe\Desktop\Trabalho.txt
 //2a parte ->alocar informações em vetores agora.
