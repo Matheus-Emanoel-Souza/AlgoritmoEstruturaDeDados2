8fba21698c99048004ed3c87f81bbd2ad6f463f7
Matheus Emanoel <matheusemanoel@ucl.br>
2025-04-27 18:48:20 -0300

    #Tested-by: Matheus Emanoel
    Caminho corrigido, como o caminho está sendo repassado para multifunções, precisa passar o caminho exato.
    Então foi feita uma linha de código para achar o projeto e depois complementar ao caminho relativo.

--- Trabalho/src/Main.java
+++ Trabalho/src/Main.java
@@ -6,8 +6,11 @@ import PT2.TxtAlunosListaAlunos;
 public class Main {
     public static void main(String[] args) {
 
+    	//ystem.out.println(new File("").getAbsolutePath());
+
     	lista<Aluno> ListaAlunos = new lista<Aluno>();
-    	String CaminhoAlunos = "TXT/Alunos.txt";
+    	String caminhoBase = System.getProperty("user.dir");
+    	String CaminhoAlunos = caminhoBase + "\\src\\TXT\\Alunos.txt";
     	
     	TxtAlunosListaAlunos FormandoLista = new TxtAlunosListaAlunos();
     	ExibirPT2 exibir = new ExibirPT2();
